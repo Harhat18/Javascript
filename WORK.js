a159704fff70cb2fd5c7989719b2f2b1e451493c
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-10-03 20:46:53 +0300

    ADD

---
+++ WORK.js
@@ -0,0 +1,26 @@
+let examGrade = prompt("puan giriniz");
+let textInfo;
+
+if (examGrade >= 0 && examGrade <= 100) {
+  if (examGrade >= 90) {
+    textInfo = "AA";
+  } else if (examGrade >= 85) {
+    textInfo = "BA";
+  } else if (examGrade >= 80) {
+    textInfo = "BB";
+  } else if (examGrade >= 75) {
+    textInfo = "CB";
+  } else if (examGrade >= 70) {
+    textInfo = "CC";
+  } else if (examGrade >= 65) {
+    textInfo = "DC";
+  } else if (examGrade >= 60) {
+    textInfo = "DD";
+  } else if (examGrade >= 50) {
+    textInfo = "FD";
+  } else if (examGrade < 50) {
+    textInfo = "FF";
+  }
+}
+
+console.log(textInfo);
